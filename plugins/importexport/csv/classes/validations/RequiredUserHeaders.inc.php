885f7a4acaac1f4cdfeb310518f408153a338314
Guilherme Godoy <guilhermehlgodoy22@gmail.com>
2025-06-16 13:02:49 +0100

    Add CSV importexport plugin for OJS 3.3

---
+++ plugins/importexport/csv/classes/validations/RequiredUserHeaders.inc.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @file plugins/importexport/csv/classes/validations/RequiredUserHeaders.php
+ *
+ * Copyright (c) 2014-2025 Simon Fraser University
+ * Copyright (c) 2003-2025 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ *
+ * @class RequiredUserHeaders
+ *
+ * @ingroup plugins_importexport_csv
+ *
+ * @brief Class to validate headers in the user CSV files
+ */
+
+namespace PKP\Plugins\ImportExport\CSV\Classes\Validations;
+
+class RequiredUserHeaders
+{
+    static $userHeaders = [
+        'journalPath',
+        'firstname',
+        'lastname',
+        'email',
+        'affiliation',
+        'country',
+        'username',
+        'tempPassword',
+        'roles',
+        'reviewInterests',
+        'subscriptionType',
+        'startDate',
+        'endDate'
+    ];
+
+    static $userRequiredHeaders = [
+        'journalPath',
+        'firstname',
+        'lastname',
+        'email',
+        'roles',
+    ];
+
+    static $subscriptionHeaders = [
+        'subscriptionType',
+        'startDate',
+        'endDate'
+    ];
+
+    /**
+     * Validates whether the row contains all headers.
+	 *
+	 * @param array $row
+	 *
+	 * @return bool
+     */
+    public static function validateRowHasAllFields($row)
+    {
+        return count($row) === count(self::$userHeaders);
+    }
+
+    /**
+     * Validates whether the row contains all required headers.
+	 *
+	 * @param object $row
+	 *
+	 * @return bool
+     */
+    public static function validateRowHasAllRequiredFields($row)
+    {
+        foreach(self::$userRequiredHeaders as $requiredHeader) {
+            if (!$row->{$requiredHeader}) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Validates if the row has the necessary subscription fields when a subscription_type_id is provided
+     *
+     * @param object $row
+     * @return bool
+     */
+    public static function validateSubscriptionFields($row)
+    {
+        $hasSubscriptionType = !empty($row->subscriptionType);
+        $hasStartDate = !empty($row->startDate);
+        $hasEndDate = !empty($row->endDate);
+
+		return (!$hasSubscriptionType && !$hasStartDate && !$hasEndDate)
+			|| ($hasSubscriptionType && $hasStartDate && $hasEndDate);
+    }
+}
